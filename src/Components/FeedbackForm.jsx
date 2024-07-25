9a50a3d1ab7f5bf681ed9e347ae4a85f38ba4004
sherelynn <sherelynnb@missionreadyhq.com>
2024-07-26 11:26:29 +1200

    Create basic template

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import './FeedbackForm.css'; // Import CSS for styling
+import React, { useState } from 'react'
+import './FeedbackForm.css' // Import CSS for styling
 
 const FeedbackForm = () => {
-
   return (
     <>
-    <nav>
-    Tell Us What You Think
-    </nav>
+      <nav>Tell Us What You Think</nav>
       <form className="feedback-form">
         <h2>We'd Love to Hear From You!</h2>
         <p>Please share your feedback with us.</p>
-        
+
+        <input type="text" name="name" placeholder="Your name" />
+        <input type="email" name="email" id="email" placeholder="Your email" />
+
+        <textarea
+          name="feedback"
+          id="feedback"
+          placeholder="Message"
+        ></textarea>
+
+        <button type="submit">Submit Feedback</button>
       </form>
     </>
-  );
-};
+  )
+}
 
-export default FeedbackForm;
+export default FeedbackForm
